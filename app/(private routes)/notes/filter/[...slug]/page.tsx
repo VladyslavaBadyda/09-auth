c55b8b5bcba647a9fadeb0fddcba156ad372fc1b
Vladyslava Badyda <vlada87.com@gmail.com>
2026-04-21 15:57:45 +0300

    new-homework-fix error

--- app/(private routes)/notes/filter/[...slug]/page.tsx
+++ app/(private routes)/notes/filter/[...slug]/page.tsx
@@ -2,25 +2,24 @@
 
 import { fetchNotes } from "@/lib/api";
 import NotesClient from "./Notes.client";
-import { Metadata } from "next";
 import {
   dehydrate,
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
-import { usePathname } from 'next/navigation';
-
+const { slug } = await params;
 interface Props {
   params: Promise<{ slug: string[] }>;
 }
 
-export default async function NotesPage({ params }: Props) {
+export default function NotesPage({ params }: Props) {
   const queryClient = new QueryClient();
 
-  const { slug } = await params;
+  const { slug } = params;
   const rawTag = slug?.[0];
   const tag = rawTag === "all" ? undefined : rawTag;
-  await queryClient.prefetchQuery({
+
+  queryClient.prefetchQuery({
     queryKey: ["note", slug],
     queryFn: () => fetchNotes(tag),
   });
@@ -30,30 +29,4 @@ export default async function NotesPage({ params }: Props) {
       <NotesClient tag={tag} />
     </HydrationBoundary>
   );
-}
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { slug } = await params;
-  const rawTag = slug?.[0];
-  const tag = rawTag === "all" ? undefined : rawTag;
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-
-  return {
-    title: `Notes filtered by:  ${tag}`,
-    description: `Browse notes filtered by "${tag}" tag. Find relevant notes quickly.`,
-    openGraph: {
-      title: `Notes filtered by:  ${tag}`,
-      description: `Browse notes filtered by "${tag}" tag. Find relevant notes quickly.`,
-      url: `${baseUrl}/notes/filter/${tag}`,
-      images: [
-        {
-          url: "/notehub-og-meta.jpg",
-          width: 1200,
-          height: 600,
-          alt: "image with app preview",
-        },
-      ],
-      type: "article",
-    },
-  };
 }
